9b539fbe168a06b8f94b5f49b9a40b3675b95b3e
fontcuberta <isafontcu@gmail.com>
2017-01-09 11:09:49 +0100

    Add starter code to solution

---
+++ starter_code/elevator.js
@@ -0,0 +1,39 @@
+class Elevator {
+  constructor(){
+    this.floor      = 0;
+    this.MAXFLOOR   = 5;
+    this.requests   = [];
+    this.idle       = true; //not serving any request at the moment
+    this.currentReq = null;
+  }
+
+  start(){
+
+  }
+
+  stop(){
+
+  }
+
+  update(){
+
+  }
+
+  floorUp() {
+
+  }
+
+  floorDown() {
+
+  }
+
+  call(floor) {
+
+  }
+
+  log() {
+  
+  }
+}
+
+module.exports = Elevator;
---
+++ starter_code/index.js
@@ -0,0 +1,5 @@
+const Elevator = require('./elevator.js');
+
+class ElevatorManager {
+
+}
