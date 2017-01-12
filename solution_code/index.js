61545d0a0de3eded1919fc9a908897743e1ec575
Fer Martin <fernandomartinsantana@gmail.com>
2017-01-12 13:14:33 +0000

    Update index.js

--- solution_code/index.js
+++ solution_code/index.js
@@ -1,8 +1,6 @@
 const Person = require('./person.js');
 const Elevator = require('./elevator.js');
-const _ = require('lodash');
 
-// let pedro = new Person("Pedro", 1, 5);
 let isa  = new Person("Isa",  2, 6);
 let raul = new Person("Raul", 6, 3);
 let fer  = new Person("Fer",  5, 1);
@@ -11,23 +9,8 @@ let gonzu = new Person("Gonzu",  7, 4);
 let elevator = new Elevator();
 elevator.start();
 
-// elevator.call(pedro);
 elevator.call(isa);
 elevator.call(raul);
 elevator.call(fer);
 
-// elevator.call({at: 5, to: 3});
-
 setTimeout(() => {elevator.call(gonzu)}, 10000);
-
-// let rafael = new Person("Rafael", 5);
-// let sara = new Person("Sara", 4);
-//
-// let floors = [];
-//
-// for(let i = 0;i<10;i++){
-//   floors.push(new Floor(i));
-// }
-//
-// floors[2].addPerson(rafael);
-// floors[0].addPerson(sara);
